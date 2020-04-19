1b7082d29fc0c82d57db04c2e951b91b14683153
PDA-Khacer <59501767_PDA_Khacer@users.noreply.github.com>
2020-04-19 10:30:37 +0700

    add form
    thêm giao diện

---
+++ BTL_CSDL/AdminWeb/Content/js/scritpmain.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    
+
+    $(".input").click(function () {
+        alert("test!");
+    });
+
+    $("#select-fied").change(function () {
+        var x = document.getElementById("select-fied");
+        var y;
+        var num = 0;
+        while (num < x.length) {
+            if(num <10)
+                y = document.getElementById("ce_0" + num);
+            else
+                y = document.getElementById("ce_" + num);
+            $(y).css("visibility","hidden");
+            num++;
+        }
+        var i = x.selectedIndex;
+        var z;
+        if (i < 10)
+            z = document.getElementById("ce_0" + i);
+        else
+            z = document.getElementById("ce_" + i);
+        $(z).css("visibility","initial");
+    });
+});
